bb8b766b9c43da17bbf0e4124125a1dc94f3fd2e
Cy Ellison <dragonz2626@gmail.com>
2018-09-24 13:17:46 -0600

    addedd more stuff

--- FirstProject.playground/Contents.swift
+++ FirstProject.playground/Contents.swift
@@ -5,3 +5,14 @@ import UIKit
 var firstString = "🅱️ O I 😂😂😂😂😂😂😂😂😂😂😂😂😂😂"
 print(firstString)
 
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+let explicitFloat: Float = 70.5
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+
+
