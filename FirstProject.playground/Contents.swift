eb9bdb4904c5183ffe1b87c9de0329dbe39e36c3
Cy Ellison <dragonz2626@gmail.com>
2018-09-26 12:15:55 -0600

    added more of my swift tour

--- FirstProject.playground/Contents.swift
+++ FirstProject.playground/Contents.swift
@@ -46,25 +46,79 @@ occupations["Jayne"] = "public Relations"
 shoppinglist.append("blue paint")
 print(shoppinglist)
 
-//funtions
-func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
-    var min = scores[0]
-    var max = scores[0]
-    var sum = 0
-    
-    for score in scores {
-        if score > max {
-            max = score
-        } else if score < min {
-            min = score
-        }
-        sum += score
+
+
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
     }
     
-    return (min, max, sum)
+}
+print(teamScore)
+
+var optionalString: String? = "HELLO"
+print(optionalString == nil)
+//Prints false
+
+var optionalName: String? = "John Appleseed"
+var greeting = "HELLO!!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+print(informalGreeting)
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants in a log")
+    break
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+    break
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+    break
+default:
+        print("everything tastes good in soup")
+    
+}
+
+let interestingNumbers = [
+    "Prime": [2,3,5,7,11,13],
+    "Fibonacci": [1,1,2,3,5,8],
+    "Squaare": [1,4,9,16,25],
+]
+var largest = 0
+for (kind, numbers) in  interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
         }
-let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
-print(statistics.sum)
+    }
+}
+print(largest)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+}while m < 100
+print(m)
+
 
 
 
